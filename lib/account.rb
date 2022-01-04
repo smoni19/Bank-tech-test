c01f0b4c059f3d9bd4c1e1f9adadf1d39b351a57
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2022-01-04 17:22:17 +0000

    Added deposit method which takes an amount and adds to balance

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
+
+# Class to manage all account functionality
 class Account
   attr_reader :balance
 
-  def new
-    @balanace = 0
+  def initialize
+    @balance = 0
   end
-  
+
+  def deposit(amount)
+    @balance += amount
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,8 +1,18 @@
+# frozen_string_literal: true
+
 require 'account'
 
 describe 'account' do
+  before do
+    @account = Account.new
+  end
+
   it 'starts with a balance of 0' do
-    account = Account.new
-    expect(account.new).to eq 0
+    expect(@account.balance).to eq 0
+  end
+
+  it 'should increase balance when #deposit is called with an amount' do
+    @account.deposit(1000)
+    expect(@account.balance).to eq 1000
   end
 end
