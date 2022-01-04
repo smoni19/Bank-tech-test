d9a316aadc6121b21a050585821b84fe4471c546
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2022-01-04 20:00:50 +0000

    All basic functionality implemented

--- lib/account.rb
+++ lib/account.rb
@@ -2,26 +2,37 @@
 
 # Class to manage all account functionality
 class Account
-  attr_reader :balance, :transaction_history, :transaction
+  attr_reader :balance, :statement, :transaction
 
   def initialize
     @balance = 0
-    @transaction_history = []
+    @statement = []
   end
 
   def deposit(credit, date)
     @balance += credit
-    date = date.gsub('-','/') 
-    add_to_transaction_history("#{date} || #{credit} || || #{balance}")
+    date = date.gsub('-', '/')
+    add_to_statement("#{date} || #{format('%.2f', credit)} || || #{format('%.2f', balance)}")
   end
 
   def withdraw(debit, date)
     @balance -= debit
-    date = date.gsub('-','/') 
-    add_to_transaction_history("#{date} || || #{debit} || #{balance}")
+    date = date.gsub('-', '/')
+    add_to_statement("#{date} || || #{format('%.2f', debit)} || #{format('%.2f', balance)}")
   end
 
-  def add_to_transaction_history(transaction)
-    @transaction_history << transaction
+  def add_to_statement(transaction)
+    @statement << transaction
+  end
+
+  def print_statement
+    statement_to_print = []
+    @statement.reverse_each do |entry|
+      statement_to_print << entry
+    end
+    puts 'date || credit || debit || balance'
+    statement_to_print.each do |entry|
+      puts entry
+    end
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -11,23 +11,22 @@ describe Account do
     expect(@account.balance).to eq 0
   end
 
-  it 'should start with an empty transaction_history' do
-    expect(@account.transaction_history.length).to eq 0
+  it 'should start empty' do
+    expect(@account.statement.length).to eq 0
   end
 end
 
 describe '#deposit' do
   before do
     @account = Account.new
+    @account.deposit(1000.00, '10-01-2023')
   end
 
-  it 'should increase transaction_history length by 1 when a deposit is made' do
-    @account.deposit(1000, '10-01-2023')
-    expect(@account.transaction_history.length).to eq 1
+  it 'should increase statement length by 1 when a deposit is made' do
+    expect(@account.statement.length).to eq 1
   end
 
   it 'should take an amount and date argument and increase balance' do
-    @account.deposit(1000, '10-01-2023')
     @account.deposit(2000, '13-01-2023')
     expect(@account.balance).to eq 3000
   end
@@ -36,34 +35,47 @@ end
 describe '#withdraw' do
   before do
     @account = Account.new
-    @account.deposit(1000, '10-01-2023')
+    @account.deposit(1000.00, '10-01-2023')
   end
 
   it 'should take an amount and date argument and decrease balance' do
-    @account.withdraw(1000, '10-01-2023')
+    @account.withdraw(1000.00, '10-01-2023')
     expect(@account.balance).to eq 0
   end
 end
 
-describe '#add_to_transaction_history' do
+describe '#add_to_statement' do
   before do
     @account = Account.new
+    @account.deposit(1000.00, '10-01-2023')
   end
 
-  it 'should add deposity and withdrawal information to transcation_history' do
-    @account.deposit(1000, '10-01-2023')
-    @account.withdraw(400, '10-01-2023')
-    expect(@account.transaction_history).to eq ['10/01/2023 || 1000 || || 1000', '10/01/2023 || || 400 || 600']
+  it 'should add deposit and withdrawal information to transcation_history' do
+    expect(@account.statement).to eq ['10/01/2023 || 1000.00 || || 1000.00']
   end
 
-  it 'should increase transaction_history length by 1 when a deposit is made' do
-    @account.deposit(1000, '10-01-2023')
-    expect(@account.transaction_history.length).to eq 1
+  it 'should increase statement length by 1 when a deposit is made' do
+    expect { @account.deposit(1000.00, '10-01-2023') }.to change { @account.statement.length }.by(1)
   end
 
-  it 'should increase transaction_history length by 1 when a withdrawal is made' do
-    @account.deposit(1000, '10-01-2023')
-    @account.withdraw(1000, '10-01-2023')
-    expect(@account.transaction_history.length).to eq 2
+  it 'should increase statement length by 1 when a withdrawal is made' do
+    expect { @account.withdraw(1000.00, '10-01-2023') }.to change { @account.statement.length }.by(1)
+  end
+end
+
+describe '#print_statement' do
+  before do
+    @account = Account.new
+    @account.deposit(1000.00, '10-01-2023')
+    @account.deposit(2000.00, '13-01-2023')
+    @account.withdraw(500.00, '14-01-2023')
+  end
+
+  it 'should return information about all transactions made on the account with headers' do
+    expect(@account.print_statement).to be
+    ['date || credit || debit || balance',
+     '14/01/2023 || || 500.00 || 2500.00',
+     '13/01/2023 || 2000.00 || || 3000.00',
+     '10/01/2023 || 1000.00 || || 1000.00']
   end
 end
